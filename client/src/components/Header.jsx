1c372d12676afa39781ca274637f7714e7408e53
Fernando Garcia <odnanref@kubrick-5995.local>
2021-08-07 14:49:07 -0700

    remove a bunch of junk

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react'
 
 const Header = ({ }) => {
     return (
-        <div className="decoration-clone bg-gradient-to-b from-yellow-400 to-red-500 text-transparent">
+        <div>
         <div className="grid justify-items-end">
-                <div className="decoration-clone bg-gradient-to-b from-yellow-400 to-red-500 text-transparent flex items-stretch font-sans md:font-serif text-4xl ml-96 self-auto">
+                <div className="flex items-stretch font-sans md:font-serif text-4xl ml-96 self-auto">
             <p>Cachetons</p>
                 <p className="pl-96 font-sans md:font-serif text-4xl self-auto">The only user to user marketplace</p>
             </div>
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import logReducer from "./logReducer";
-
-
-export default combineReducers({
-    log: logReducer
-});
-
