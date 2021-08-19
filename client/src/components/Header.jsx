c4892dac5fbd3e9fe2376f3b29177d791d4939a5
Fernando Garcia <odnanref@kubrick-5995.local>
2021-08-18 22:59:01 -0700

    clean up and push

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from 'react'
 const Header = ({ }) => {
     return (
 
-        <div className="mt-8 grid">
+        <div className="grid">
             <div className="flex items-stretch font-sans md:font-serif text-4xl mt-8 ml-96 self-auto">
                 <p>Cachetons</p>
-                <p className="pl-96 mt-8 font-sans md:font-serif text-2xl self-auto">Search through some of your favorite user to user marketplaces</p>
+                <p className="pl-96 mt-2 font-sans md:font-serif text-2xl self-auto">Search through some of your favorite user to user marketplaces</p>
             </div>
         </div>
 
