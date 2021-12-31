43efcf6fff80193bf732bcdd5b790a4a7637fcbb
Fernando Garcia <odnanref@Kubrick-MaxPro.local>
2021-12-30 17:05:13 -0800

    pushing fixes to setup.py

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,6 +7,8 @@ const Header = ({ }) => {
         <div>
             <div className="font-sans md:font-serif text-4xl">Cachetons</div>
             <div className="font-sans pt-10 md:font-serif text-2xl" style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <br>
+                </br>
                 Search through some of your favorite user to user marketplaces
             </div>
         </div>
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-from src.version import __version__
+from api.__version__ import version
 
 exclude_dirs = ["venv", "ENV"]
 
@@ -8,42 +8,42 @@ with open("README.md") as f:
     readme = f.read()
 
 production_requires = [
-    "appdirs==1.4.4",
-    "lxml==4.6.3",
-    "beautifulsoup4==4.9.3",
-    "black==21.4b0",
-    "bs4==0.0.1",
-    "certifi==2020.12.5",
-    "chardet==4.0.0",
-    "click==7.1.2",
-    "fastapi==0.63.0",
-    "h11==0.12.0",
-    "httptools==0.1.1",
-    "idna==2.10",
-    "mypy-extensions==0.4.3",
-    "pathspec==0.8.1",
-    "pydantic==1.8.1",
-    "python-dotenv==0.17.0",
-    "PyYAML==5.4.1",
-    "regex==2021.4.4",
-    "requests==2.25.1",
-    "soupsieve==2.2.1",
-    "starlette==0.13.6",
-    "toml==0.10.2",
-    "typing-extensions==3.7.4.3",
-    "urllib3==1.26.4",
-    "uvicorn==0.13.4",
-    "uvloop==0.15.2",
-    "watchgod==0.7",
-    "websockets==8.1",
-    "selenium==3.141.0",
+    "appdirs",
+    "lxml",
+    "beautifulsoup4",
+    "black",
+    "bs4",
+    "certifi",
+    "chardet",
+    "click",
+    "fastapi",
+    "h11",
+    "httptools",
+    "idna",
+    "mypy-extensions",
+    "pathspec",
+    "pydantic",
+    "python-dotenv",
+    "PyYAML",
+    "regex",
+    "requests",
+    "soupsieve",
+    "starlette",
+    "toml",
+    "typing-extensions",
+    "urllib3",
+    "uvicorn",
+    "uvloop",
+    "watchgod",
+    "websockets",
+    "selenium",
 ]
 
 dev_requires = ["black", "flake8"]
 
 setup(
     name="Cachetons",
-    version=__version__,
+    version=version,
     author="Fernando Garcia",
     author_email="odnanrefdev@gmail.com",
     license="None I guess",
